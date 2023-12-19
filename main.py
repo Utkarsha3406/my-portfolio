f5aa3214e368871c68f00b45763be6858e7fe451
Utkarsha3406 <up3406@srmist.edu.in>
2023-12-19 17:20:13 +0530

    Add app description image and links

--- main.py
+++ main.py
@@ -16,13 +16,19 @@ with col2:
     st.info(content)
 
 st.write("Below you can find some of the apps I have built in python.Feel free to contact me")
-col3,col4=st.columns(2)
+col3,empty_col,col4=st.columns([1.5,0.5,1.5])
 
 df=pandas.read_csv("data.csv",sep=";")
 with col3:
     for index ,row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index ,row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
