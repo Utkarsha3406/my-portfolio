34efa46fe9c202c1442a3f9f0d2a343a8cb00c6a
Utkarsha3406 <up3406@srmist.edu.in>
2023-12-19 17:07:21 +0530

    render titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas
+#pandas is third pardy libraries used to read csv data
 st.set_page_config(layout="wide")
 col1,col2 =st.columns(2)
 
@@ -13,3 +15,14 @@ with col2:
     """
     st.info(content)
 
+st.write("Below you can find some of the apps I have built in python.Feel free to contact me")
+col3,col4=st.columns(2)
+
+df=pandas.read_csv("data.csv",sep=";")
+with col3:
+    for index ,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index ,row in df[10:].iterrows():
+        st.header(row["title"])
