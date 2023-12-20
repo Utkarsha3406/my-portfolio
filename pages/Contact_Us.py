82659849e9219671b6ccfb021f0be0af691c9fe1
Utkarsha3406 <up3406@srmist.edu.in>
2023-12-20 16:23:48 +0530

    Send Email to self

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.header("Contact Me")
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address")
+    message =st.text_area("Your messgae")
+    button=st.form_submit_button()
+
+
